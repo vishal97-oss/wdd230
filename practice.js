78a93d9a6d1cb6e56cd4e16855ff8ce31a40ac28
vishal97 -oss <vai19004@byui.edu>
2022-09-15 19:33:01 -0600

    New

---
+++ practice.js
@@ -0,0 +1,14 @@
+let date = new Date();
+
+console.log(date);
+
+let year = date.getFullYear();
+
+console.log(year);
+
+
+let currentdate = document.lastModified;
+
+console.log(currentdate);
+
+document.querySelector("h2#currentdate").textContent = "Last updated: " + currentdate
