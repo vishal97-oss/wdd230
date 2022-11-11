97489d7fc28b9b10886b36c583dc73c9c5cbb62b
vishal97 -oss <vai19004@byui.edu>
2022-11-11 13:23:00 -0700

    Changes

--- week 9/js/prophet.js
+++ week 9/js/prophet.js
@@ -19,10 +19,21 @@ getProphets(requestURL)
 function dispplayProphet(item) {
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
+    let img = document.createElement('img');
+    let dob = document.createElement('p');
+    let pob = document.createElement('p');
     
 
     h2.textContent = item.name + ' ' + item.lastname;
+    img.setAttribute('src', item.imageurl);
+    img.setAttribute('alt', item.name + ' ' + item.lastname);
+    dob.textContent = item.birthdate;
+    pob.textContent = item.birthplace;
 
     card.appendChild(h2);
+    card.appendChild(dob);
+    card.appendChild(pob);
+    card.appendChild(img);
+
     document.querySelector('.cards').appendChild(card);
 }
