a5d9f33d7301f655ab2a2747cd0904fff98cc4f3
vishal97 -oss <vai19004@byui.edu>
2022-11-12 23:48:10 -0700

    VS

--- week9/js/directory.js
+++ week9/js/directory.js
@@ -1,1 +1,8 @@
 let requestURL = 'https://vishal97-oss.github.io/wdd230/week9/json/data.json'
+
+async function getCompanies(requestURL){
+    let response = await fetch(requestURL);
+    console.log(response);
+}
+
+console.log("Hello World")
