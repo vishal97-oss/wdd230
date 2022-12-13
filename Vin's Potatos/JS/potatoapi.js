464ac7e68fd3eb6d4e88d780211a0feaf568a9b0
vishal97 -oss <vai19004@byui.edu>
2022-12-13 12:53:02 -0700

    Cart

---
+++ Vin's Potatos/JS/potatoapi.js
@@ -0,0 +1,31 @@
+const PotatoapiURL = "https://www.commodities-api.com/api/latest?access_key=pqmrs4b2d108by9rudvmld8wjzqsg3te47hj8nc762ro6wakch5rhxlpml2d&base=USD&symbols=RICE";
+
+const getApi = async () => {
+    const response = await fetch(PotatoapiURL);
+    const jsObject = await response.json();
+    console.log(jsObject);
+    console.log(16*jsObject.data.rates.RICE);
+    document.getElementById("API").textContent = Math.round(16*jsObject.data.rates.RICE);
+}
+getApi();
+let documentItem = document.getElementById("lbs");
+let usDelivery = document.getElementById("Delivery");
+let usPickup = document.getElementById("Pickup");
+console.log(usDelivery)
+
+usDelivery.addEventListener("focusin", Calc);
+
+let lbs = document.getElementById("lbs").value;
+function Calc(){
+    answer == lbs * 1
+    console.log(answer)
+}
+
+
+// let documentItem = document.getElementById('localstorage');
+// let lbs = document.getElementById('lbs');
+
+
+// if(localStorage.getItem('addtoCart')){
+//     localStorage.setItem("addtoCart",lbs.value)
+// }
